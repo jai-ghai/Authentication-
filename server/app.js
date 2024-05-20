56d295325ad1d9daabbde98e9351674fa880b69b
Jai ghai <63393416+jai-ghai@users.noreply.github.com>
2024-05-20 21:52:57 +0530

    frontend

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "https://authentication-f.vercel.app/",
+    origin: "https://authentication-f.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
