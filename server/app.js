b3bade2bd34591b03f8493e0420faa0f6fb71ae7
Jai ghai <63393416+jai-ghai@users.noreply.github.com>
2024-05-20 21:28:23 +0530

    deploy

--- server/app.js
+++ server/app.js
@@ -32,10 +32,6 @@ app.use("/api/v1", user);
 
 app.use(ErrorMiddleware);
 
-app.get("/", (req, res) =>
-  res.send(
-    `<h1>Site is Working. click <a href=${process.env.FRONTEND_URL}>here</a> to visit frontend.</h1>`
-  )
-);
+app.get("/", (req, res) => res.send(`<h1>Site is Working. `));
 
 export default app;
